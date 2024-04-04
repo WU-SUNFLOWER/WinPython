ec3230876d8f8900ec61c46aa84b293e1c61513c
WU-SUNFLOWER <47438910+WU-SUNFLOWER@users.noreply.github.com>
2024-04-04 15:17:51 +0800

    增加外挂dll

--- main.cpp
+++ main.cpp
@@ -3,14 +3,47 @@
 #include "interpreter.hpp"
 #include "Universe.hpp"
 #include <cstdlib>
+#include <Windows.h>
+#include <filesystem>
+
+typedef void(*Convert_Func)(const char*, const char*);
 
 int main(int argc, char* argv[]) {
-    if (argc <= 1) {
-        printf("vm need a parameter as filename");
+    std::string byteCodeFile;
+    
+    // 传入.py文件
+    if (argc == 2) {
+        // 加载dll文件
+        HINSTANCE dllHandle = LoadLibrary("python27_d.dll");
+        if (!dllHandle) {
+            printf("Fail to load python27_d.dll");
+            exit(-1);
+        }
+        // 加载转换函数
+        Convert_Func converter = 
+            reinterpret_cast<Convert_Func>(GetProcAddress(dllHandle, "ConvertCodeStringToByteCode"));
+        if (!converter) {
+            printf("Fail to load ConvertCodeStringToByteCode");
+            exit(-1);
+        }
+        // 执行转换
+        const char* sourceFile = argv[1];
+        byteCodeFile =
+            std::filesystem::path(sourceFile).replace_extension("pyc").string();
+    }
+    // 传入.pyc文件
+    else if (argc == 3 && strcmp(argv[1], "-c") == 0) {
+        byteCodeFile = argv[2];
+    }
+    else {
+        puts("Invalid arguments. Use either:\n");
+        puts("WinPython <python source code filename>");
+        puts("WinPython -c <python bytecode filename>");
         exit(-1);
     }
+
     Universe::genesis();
-    BufferedInputStream stream(argv[1]);
+    BufferedInputStream stream(byteCodeFile.c_str());
     ByteCodeFileParser parser(&stream);
     CodeObject* code = parser.parse();
     Interpreter interpreter;
